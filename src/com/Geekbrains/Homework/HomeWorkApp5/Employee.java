5ebf9d8bd0ee7b6e42e402c21d6d289e257ae1a1
islom.m <b.madiyorov.i@mail.ru>
2022-03-28 16:59:15 +0500

    ДЗ 5

---
+++ src/com/Geekbrains/Homework/HomeWorkApp5/Employee.java
@@ -0,0 +1,78 @@
+package com.Geekbrains.Homework.HomeWorkApp5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return ("------Name " + name + "\n" +
+                "Position - " + position + "\n" +
+                "Email - " + email + "\n" +
+                "Phone - " + phone + "\n" +
+                "Salary - " + salary + "\n" +
+                "Age - " + age);
+
+    }
+}
